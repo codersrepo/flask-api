dacdf6367dfd757e757ae3ecdc588636a90bb153
Nabina Khadka <nabina.khadka@kotuko.it>
2022-09-11 21:24:01 +0545

    Init

---
+++ api.py
@@ -0,0 +1,28 @@
+import flask
+from flask import *
+from decision import *
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/api/predict', methods=['POST'])
+def api_all():
+
+    data = request.get_json()
+    arr = []
+    for i, item in enumerate(data):
+        tmp = [[int(data[i]['marks']), int(data[i]['lab_work']), int(data[i]['attendence']),
+                int(data[i]['assignment']), int(data[i]['internal_assessment']), ]]
+        arr.append(tmp)
+    # marks = data['marks']
+    # attendence = data['attendence']
+    # lab_work = data['lab_work']
+    # assignment = data['assignment']
+    # assessment = data['internal_assessment']
+    prediction = []
+    for a in arr:
+        tmp = make_prediction(a)
+        prediction.append(tmp)
+    return jsonify(prediction)
+
+app.run()
